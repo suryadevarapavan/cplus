5bfca534a2cf32ebb1884344cda990a05f569740
suryadevarapavan <suryadevarapavan770@gmail.com>
2026-02-28 10:48:37 +0530

    Stared game dev

---
+++ d2/enums.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+enum CharacterType{
+  Swordsman,
+  Scorcerer,
+  Demons
+};
+struct Character {
+  CharacterType tp;
+  string name;
+  int attack;
+  int heal;
+  int hp;
+};
+
+const Character Swordsmanstats = {Swordsman,"Swordsman",80,40,5};
+const Character Scorcererstats = {Scorcerer,"Scorcerer",120,10,20};
+const Character Demonsstats = {Demons,"Demons",100,20,51};
+
+int main(){
+  string n;
+  int choice;
+  Character player;
+
+  cout<<"Welcome to my game\n Enter your choice for class \n 1.Swordsman \n 2.Scorcerer \n 3.Demon \n Enter your choice:";
+  cin>>choice;
+
+  switch(choice){
+    case 1:
+      player=Swordsmanstats;
+    case 2:
+      player=Scorcererstats;
+    case 3:
+      player=Demonsstats;
+    default:
+      cout<<"Error!";
+  }
+
+  //cout<<"Your character name is "<<n<<" and stats are:"<<endl;
+  return 0;
+}
